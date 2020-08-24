b0a22c3328dc701c422f5585ebbff1bb8cb2fab7
Tooraj Taraz <tooraj.info@gmail.com>
2020-08-24 16:19:23 +0430

    finish writing printing in colors

--- print_color.c
+++ print_color.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include "./ansi_code.h"
-void generic_print(int indent, char *type, char *name){
+#include "./print_color.h"
 
+void generic_print(int indent, char *type, char *name){
+   if(type[0] == 'D'){
+        printf("%*s[%s]\n", indent, "", name);
+   }
+  else {
+        printf("%*s-|%s| %s\n", indent, "", type, name);
+   } 
 }
 void white_print(int indent, char *type, char *name){
     printf(WHT);
@@ -20,6 +27,11 @@ void green_print(int indent, char *type, char *name){
 }
 void blue_print(int indent, char *type, char *name){
     printf(BLU);
+    generic_print(indent, type, name);
+    printf(reset);
+}
+void bold_print(int indent, char *type, char *name){
+    printf(BHWHT);
     generic_print(indent, type, name);
     printf(reset);
 }
